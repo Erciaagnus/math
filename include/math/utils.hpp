2503ce6ae56e316d9b2ce36fb54502f469c96dbc
Erciaagnus <henricus0973@gmail.com>
2024-01-09 03:38:22 +0900

    codes for math utils

---
+++ include/math/utils.hpp
@@ -0,0 +1,15 @@
+// Copyright (C) 2024 Programmers, All Rights Reserved
+//Authors: Lim Jeong Hyeok
+#ifndef PROGRAMMERS__MATH__UTILS_HPP_
+#define PROGRAMMERS__MATH__UTILS_HPP_
+
+namespace holyground::mat {
+
+// 사칙연산
+// Add, Substract, Multiply, Divide
+
+// 비교연산
+// IsEqual
+} // namespace holyground::mat
+
+#endif // PROGRAMMERS__MATH__UTILS_HPP_
---
+++ src/utils.cpp
@@ -0,0 +1,4 @@
+// Copyright (C) 2024 Programmers, All Rights Reserved
+//Authors: Lim Jeong Hyeok
+
+#include "math/utils.hpp"

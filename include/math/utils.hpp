9be9dd3c9be404d16db147d8326554865b03b763
Erciaagnus <henricus0973@gmail.com>
2024-01-09 15:09:57 +0900

    Added Copyright for utils C++files

--- include/math/utils.hpp
+++ include/math/utils.hpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2024 Programmers, All Rights Reserved
-//Authors: Lim Jeong Hyeok
+// Authors: Lim Jeong Hyeok
 #ifndef PROGRAMMERS__MATH__UTILS_HPP_
 #define PROGRAMMERS__MATH__UTILS_HPP_
 
@@ -7,9 +7,14 @@ namespace programmers::math {
 
 // 사칙연산
 // Add, Substract, Multiply, Divide
+auto Add(double lhs, double rhs) -> double;
+auto Subtract(double lhs, double rhs) -> double;
+auto Multiply(double lhs, double rhs) -> double;
+auto Divide(double lhs, double rhs) -> double;
 
 // 비교연산
 // IsEqual
-} // namespace holyground::mat
+auto IsEqual(double lhs, double rhs) -> bool;
+} // namespace programmers::math
 
 #endif // PROGRAMMERS__MATH__UTILS_HPP_
